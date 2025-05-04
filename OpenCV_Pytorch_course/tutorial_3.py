eb1c7935753a125d957a5581d70926fc5c7c9a15
rfeiglew <r.feiglewicz@gmail.com>
2025-05-04 13:41:11 +0200

    Tutorial 3 is now completed

---
+++ OpenCV_Pytorch_course/tutorial_3.py
@@ -0,0 +1,332 @@
+
+import torch
+from torchvision import datasets, transforms
+
+# Load the Fashion MNIST dataset (without normalization)
+dataset = datasets.FashionMNIST(root='./data',train=True, download=True, transform=transforms.ToTensor())
+
+# Stack all images into a single tensor and compute mean and std
+all_pixels = torch.cat([img.view(-1) for img, _ in dataset])
+mean = all_pixels.mean().item()
+std = all_pixels.std().item()
+
+print(f"Mean: {mean:.4f}, Std: {std:.4f}")
+
+import torch
+from torch import nn
+import torch.nn.functional as F 
+import torch.optim as optim 
+from torchinfo import summary
+
+import torchvision
+from torchvision import datasets, transforms
+
+import matplotlib.pyplot as plt 
+import numpy as np
+import random
+import time
+
+# Set seed for reproducibility
+
+def set_seeds():
+    # set random seed value
+    SEED_VALUE = 42
+
+    random.seed(SEED_VALUE)
+    np.random.seed(SEED_VALUE)
+    torch.manual_seed(SEED_VALUE)
+    # Fix seed to make training deterministic
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(SEED_VALUE)
+        torch.cuda.manual_seed_all(SEED_VALUE)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+
+set_seeds()
+
+#---------------------------------------------------------------
+#Prepare the DataLoader
+
+# Step 1: Download the training set without normalization
+raw_transform = transforms.Compose([transforms.ToTensor()])
+train_set_raw = datasets.FashionMNIST(root="F_MNIST_data", download=True, train=True, transform=raw_transform)
+
+# Step 2: Compute mean and std from the training set
+all_pixels = torch.cat([img.view(-1) for img, _ in train_set_raw])
+mean = all_pixels.mean().item()
+std = all_pixels.std().item()
+
+print(f"Computed Mean: {mean:.4f}, Computed Std: {std:.4f}")
+
+# Step 3: Define the new transform using the computed mean and std
+transform = transforms.Compose([
+    transforms.ToTensor(), #Convert to torch tensor
+    transforms.Normalize((mean,), (std,)) # Normalize
+])
+
+# Step 4 Reload datasets with proper normalization
+train_set = datasets.FashionMNIST(root="F_MNIST_data", download=True, train=True, transform=transform)
+val_set = datasets.FashionMNIST(root="F_MNIST_data", download=True, train=False, transform=transform)  # Test set
+
+print("Total Train Images:", len(train_set))
+print("Total Val Images:", len(val_set))
+
+# Creating dataloader
+train_loader = torch.utils.data.DataLoader(train_set, shuffle = True, batch_size = 64)
+val_loader = torch.utils.data.DataLoader(val_set, shuffle = False, batch_size = 64)
+
+
+# Class to idx mapping
+class_mapping = {
+    0: "T-shirt/top",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle boot"  }
+
+#------------------------------
+# Dataset Visualization
+
+def visualize_images(trainloader, num_images=20):
+    fig = plt.figure(figsize=(10,10))
+
+    # Iterate over  the first batch
+    images, labels = next(iter(train_loader))
+
+    # To calculate the number of rows and columns for subplots
+    num_rows = 4
+    num_cols = int(np.ceil(num_images / num_rows))
+
+    for idx in range(min(num_images, len(images))):
+        image,label = images[idx], labels[idx]
+
+        ax = fig.add_subplot(num_rows, num_cols, idx+1, xticks=[], yticks=[])
+        ax.imshow(np.squeeze(image), cmap="gray")
+        ax.set_title(f"{label.item()}:{class_mapping[label.item()]}")
+    
+    fig.tight_layout()
+    plt.show()
+
+visualize_images(train_loader, num_images=16)
+
+
+class MLP(nn.Module):
+    def __init__(self, num_classes):
+            super().__init__()
+            self.fc0 = nn.Linear(784, 512)
+            self.bn0 = nn.BatchNorm1d(512)
+            self.fc1 = nn.Linear(512,256)
+            self.bn1 = nn.BatchNorm1d(256)
+            self.fc2 = nn.Linear(256, 128)
+            self.bn2 = nn.BatchNorm1d(128)
+            self.fc3 = nn.Linear(128, 64)
+            self.bn3 = nn.BatchNorm1d(64)
+            self.fc4 = nn.Linear(64,num_classes)           
+            
+            self.dropout = nn.Dropout(p=0.3)
+
+    def forward(self, x):
+        #Flatten the input tensor
+        x = x.view(x.shape[0], -1) #(Batch_size, 784) --> 28x28 = 784
+        # First fully connected layer with ReLu, batch norm, and dropout 
+        x = F.relu(self.bn0(self.fc0(x)))
+        x = self.dropout(x)
+
+        x = F.relu(self.bn1(self.fc1(x)))
+
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = self.dropout(x)
+        # Output layer with softmax activation
+        x = F.relu(self.bn3(self.fc3(x)))
+        x = F.log_softmax(self.fc4(x), dim = 1)
+
+        return x
+
+# Instantiate the model.
+mlp_model = MLP(num_classes = 10)
+
+# Let's look at the paremeters and the output shape after each layers.
+print(summary(mlp_model, input_size = (1,1,28,28), row_settings = ["var_names"]))
+# Display the model summary.
+
+#------------------------------
+# training configuration
+criterion = F.nll_loss # Negative Log Likelihood Loss
+optimizer = optim.Adam(mlp_model.parameters(), lr = 1e-2) #0.01
+num_epochs = 40
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+#------------------------------------------
+# Model training
+
+def train(model, trainloader, criterion, optimizer, DEVICE):
+    model.train()
+    model.to(DEVICE)
+    running_loss = 0
+    correct_predictions = 0 
+    total_samples = 0
+
+    for images, labels in trainloader:
+        images, labels = images.to(DEVICE), labels.to(DEVICE)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs.data, dim =1)
+        total_samples += labels.size(0)
+        correct_predictions += (predicted == labels).sum().item()
+
+
+    avg_loss = running_loss / len(trainloader)
+    accuracy = 100 * correct_predictions / total_samples
+    return avg_loss, accuracy
+
+#--------------------------------------------------------
+# Model evaluation
+
+def validation(model, val_loader, criterion, DEVICE):
+    model.eval()
+    model.to(DEVICE)
+
+    running_loss = 0
+    correct_predictions = 0 
+    total_samples = 0 
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images,labels = images.to(DEVICE), labels.to(DEVICE)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1) # (B, class_id)
+            total_samples += labels.size(0)
+            correct_predictions += (predicted == labels).sum().item()
+            
+    avg_loss = running_loss / len(val_loader)
+    accuracy = 100 * correct_predictions / total_samples
+    return avg_loss, accuracy
+
+#-----------------------------------------------
+# Main process
+
+def main(model, trainloader, val_loader, epochs=5, DEVICE = "cuda"):
+
+    train_losses, val_losses = [], []
+    train_accuracies, val_accuracies = [], []
+
+    for epoch in range(epochs):
+        train_loss, train_accuracy = train(model, trainloader, criterion, optimizer, DEVICE)
+        val_loss, val_accuracy = validation(model, val_loader, criterion, DEVICE)
+
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_accuracy)
+
+        print(f"Epoch {epoch+1:0>2}/{epochs} - Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}% - Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%")
+
+    # Plotting loss and accuracy
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, epochs + 1), train_losses, label='Train Loss')
+    plt.plot(range(1, epochs + 1), val_losses, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(range(1, epochs + 1), train_accuracies, label='Train Accuracy')
+    plt.plot(range(1, epochs + 1), val_accuracies, label='Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+
+main(mlp_model, train_loader, val_loader, epochs = num_epochs, DEVICE = DEVICE)
+
+
+#------------------------------------------
+# Inference result
+images, gt_labels = next(iter(val_loader))
+
+rand_idx =random.choice(range(len(images)))
+
+plt.imshow(image[rand_idx].squeeze())
+plt.title("Ground Truth Label: " + str(int(gt_labels[rand_idx])), fontsize = 12)
+plt.axis("off")
+plt.show()
+
+# Formatting
+bold = f"\033[1m"
+reset = f"\033[0m"
+
+
+mlp_model.eval()
+
+with torch.no_grad():
+     batch_outputs = mlp_model(images.to(DEVICE))
+
+prob_score_batch = batch_outputs.softmax(dim=1).cpu()
+
+prob_score_test_image = prob_score_batch[rand_idx]
+pred_cls_id = prob_score_test_image.argmax()
+
+print("Predictions for each class on the test image:\n")
+
+for idx, cls_prob in enumerate(prob_score_test_image):
+    if idx == pred_cls_id:
+       print(f"{bold}Class: {idx} - {class_mapping[idx]}, Probability: {cls_prob:.3f}{reset}")
+    else:
+       print(f"Class: {idx} - {class_mapping[idx]}, Probability: {cls_prob:.3f}")
+
+#----------------------------------------------
+# Confusion matrix
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+
+def prediction_batch(model, batch_inputs):
+    model.eval()
+
+    batch_outputs = model(batch_inputs)
+
+    with torch.no_grad():
+        batch_probs = batch_outputs.softmax(dim=1) #along num of classes dimension
+
+    batch_cls_ids = batch_probs.argmax(dim=1)
+
+    return batch_cls_ids.cpu()
+
+val_target_labels = []
+val_predicted_labels = []
+
+for image_batch, target_batch in val_loader:
+    image_batch = image_batch.to(DEVICE)
+
+    batch_pred_cls_id = prediction_batch(mlp_model, image_batch)
+
+    val_predicted_labels.append(batch_pred_cls_id)
+    val_target_labels.append(target_batch)
+
+val_target_labels = torch.cat(val_target_labels).numpy()
+val_predicted_labels = torch.cat(val_predicted_labels).numpy()
+
+cm = confusion_matrix(y_true=val_target_labels, y_pred = val_predicted_labels)
+
+plt.figure(figsize= [15,8])
+
+# Plot the confusion matrix as a heatmap.
+sn.heatmap(cm, annot=True, fmt='d', annot_kws={"size":14})
+plt.xlabel("Predicted")
+plt.ylabel("Targets")
+plt.title(f"Confusion Matrix", color="gray")
+plt.show()
